8b71a91bf8f8d145675dfa08233b8e49d9ae5ea7
pyo i seul <isl1089@naver.com>
2020-11-13 16:09:54 +0900

    2st commit

--- src/com/javaex/practice/Ex13.java
+++ src/com/javaex/practice/Ex13.java
@@ -1,9 +1,27 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력해주세요");
+		System.out.print("숫자: ");
+		
+		int x = sc.nextInt();
+		int x_00 =(x*x*x)-(9*x)+2;
+		int x_01 =(7*x+2);
+		
+		
+		if(x<=0) {
+			System.out.print("계산결과는 " + (double)x_00 + "입니다.");
+		}else {
+			System.out.print("계산결과는 " + (double)x_01 + "입니다.");
+		}
+		
+		sc.close();
 
 	}
 
